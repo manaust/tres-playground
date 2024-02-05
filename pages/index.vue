7d01a7c66228b32a7b441923a9a178f81127c38e
Manaus Transez <manaustransez@hotmail.com>
2024-02-05 21:07:45 +0100

    Add initial scene

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <p>hello world</p>
+  <div>
+    <TresCanvas clear-color=”#82DBC5” window-size>
+      <TresPerspectiveCamera />
+      <TresMesh >
+        <TresBoxGeometry :args="[1, 1, 1]" />
+        <TresMeshNormalMaterial />
+      </TresMesh>
+    </TresCanvas>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+</script>
